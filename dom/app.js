b1335799a843465482addc966c12a1c294a2a88d
leoespindula <espindulasystem.dev@gmail.com>
2022-09-22 12:25:57 -0300

    curso vue

---
+++ dom/app.js
@@ -0,0 +1,16 @@
+new Vue({
+    el:'#desafio',
+    data: {
+        nome:'Ana Silva',
+        idade: 28,
+        imagem: 'http://files.cod3r.com.br/curso-vue/vue.jpg'
+    },
+    methods:{
+        idadeMultiplicada() {
+            return this.idade * 5
+        },
+        random() {
+            return Math.random()
+        }
+    }
+})
