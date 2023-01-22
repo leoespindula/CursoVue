42c039b922be13e5435096dd6f4ad1ed7a051627
leoespindula <espindulasystem.dev@gmail.com>
2023-01-22 14:00:48 -0300

    ve

--- dom/desafio_4/app.js
+++ dom/desafio_4/app.js
@@ -1,11 +1,15 @@
 new Vue({
 	el: '#desafio',
 	data: {
+		classe1:'destaque',
+		perigo:true
 
 	},
 	methods: {
 		iniciarEfeito() {
-
+			setInterval(()=>{
+				this.classe1 = this.classe1 == 'destaque' ? 'encolher' : 'destaque'
+			},1000)
 		},
 		iniciarProgresso() {
 
