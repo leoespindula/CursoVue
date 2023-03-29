b0c8d0282b1079b93d209f8da2693ec463404800
leoespindula <espindulasystem.dev@gmail.com>
2023-03-29 16:20:21 -0300

    Matador de monstro

--- projeto-01-montro/app.js
+++ projeto-01-montro/app.js
@@ -3,7 +3,7 @@ new Vue({
     data:{
         running:false,
         playerLife:100,
-        monsterLife:0,
+        monsterLife:100,
     },
     computed:{
         hasResulted(){
@@ -12,9 +12,37 @@ new Vue({
 
     },
     methods:{
+        startGame(){
+            this.running =true
+            this.playerLife = 100
+            this.monsterLife = 100
+        },
+        attack(especial){
+            this.hurt('playerLife', 7, 12, false)
+            this.hurt('monsterLife', 5, 10, especial)
+        },
+        hurt(prop, min, max, especial){
+            const plus = especial ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[prop] = Math.max(this[prop] - hurt, 0)
+        },
+        healAndHurt(){
+            this.heal(10, 15)
+            this.hurt('playerLife', 7, 12, false)
+        },
+        getRandom(min, max){
+            const value =Math.random() * (max - min) + min
+            return Math.round(value)
+        },
+        heal(min, max){
+            const heal = this.getRandom(min, max)
+            this.playerLife = Math.min(this.playerLife + heal, 100)
+        }
 
     },
     watch:{
-
+        hasResulted(value){
+            if(value) this.running = false
+        }
     }
 })
