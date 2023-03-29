e12ba3e6c7ca7148c001b18fb28b5ac961d9471d
leoespindula <espindulasystem.dev@gmail.com>
2023-03-29 14:35:53 -0300

    Matador de monstro

--- projeto-01-montro/app.js
+++ projeto-01-montro/app.js
@@ -1,10 +1,14 @@
 new Vue({
     el:'#app',
     data:{
+        running:false,
         playerLife:100,
-        monsterLife:100,
+        monsterLife:0,
     },
     computed:{
+        hasResulted(){
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
 
     },
     methods:{
