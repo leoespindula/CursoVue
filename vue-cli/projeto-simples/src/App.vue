420829812eefa15f1dd71c9d536588a8f17a4f78
leoespindula <espindulasystem.dev@gmail.com>
2023-03-31 17:43:48 -0300

    Vue CLi

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>{{titulo}}</h1>
+    <button @click="titulo += '#'">Alterando Titulo</button>
+  </div>
+
+
+</template>
+
+<script>
+  export default {
+    data: function(){
+      return{
+        titulo:'Teste data usando Vue js'
+      }
+    }
+  }
+</script>
+
+<style>
+  #app{
+    background-color: chocolate;
+    color: #fff;
+  }
+</style>
